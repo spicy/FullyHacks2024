76a64c4ff487b12e8d4402b368b36b6abb84ecd8
PrinceSidon <siddharth.vasu.ca@gmail.com>
2024-02-25 11:13:55 -0800

    Working on bullets (not finished)

--- Assets/Scripts/EnemyBulletScript.cs
+++ Assets/Scripts/EnemyBulletScript.cs
@@ -5,25 +5,38 @@ using UnityEngine;
 public class EnemyBulletScript : MonoBehaviour
 {
     private GameObject player;
-    private Rigidbody2D rb;
+    //private Rigidbody2D rb;   // RigidBody is causing errors
     public float force;
+    private float timer;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<RigidBody2D>();
+        //rb = GetComponent<RigidBody2D>();
         player = GameObject.FindGameObjectWithTag("Player");
 
         Vector3 direction = player.transform.position - transform.position;
-        rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
+        //rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
     
         float rot = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0, 0, rot);    
+        transform.rotation = Quaternion.Euler(0, 0, rot + 90);    
     }
 
     // Update is called once per frame
     void Update()
     {
-    
+        if (timer > 10)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        timer += Time.deltaTime;
+
+        if (other.gameObject.CompareTag("Player")) {
+            Destroy(gameObject);
+        }
     }
 }
