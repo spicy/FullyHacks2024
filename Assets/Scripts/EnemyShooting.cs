2c61f4ffdf2e8c4595ac78ec1c93db36af29d64b
PrinceSidon <siddharth.vasu.ca@gmail.com>
2024-02-25 10:34:31 -0800

    Added bullet logic

---
+++ Assets/Scripts/EnemyShooting.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooting : MonoBehaviour
+{
+    public GameObject Bullet;
+    public Transform BulletPos;
+
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > 2)
+        {
+            timer = 0;
+            shoot();
+        }
+    }
+
+    void shoot() 
+    {
+        Instantiate(Bullet, BulletPos.position, Quaternion.identity);
+    }
+}
